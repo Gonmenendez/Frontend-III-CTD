360486ef8e3e2a1777aa944139b7201aa18a06a0
Gonmenendez <gnmenendez@gmail.com>
2023-04-28 18:55:50 -0300

    Uploading activities from class 7

---
+++ Clase 7/seventh-class/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from './styles/classSeven.module.css'
+
+class App extends React.Component {
+  render() {
+    return (
+    <div>
+      <h1 className={styles.title}>HOLA</h1>
+      <h1 className={styles.title}>HOLA</h1>
+      <h1 className={styles.title}>HOLA</h1>
+      <h1 className={styles.title}>MUNDO</h1>
+    </div>
+    )
+  }
+}
+
+export default App;
